2037cc2619bc444b74b0f775c49c6ba292c2560b
Mehran Hussain <mehranh91309@gmail.com>
2021-12-08 11:05:11 +0500

    something new

--- backend/Server.js
+++ backend/Server.js
@@ -12,7 +12,7 @@ process.on("uncaughtException", (err) => {
 
 
 // Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
+if (process.env.NODE_ENV !== 'production') {
     require("dotenv").config({ path: "backend/config/config.env" });
   }
 //database Connection
