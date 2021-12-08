540ca8e5e659f8532c2072c9d08157053fd7c3ae
Mehran Hussain <mehranh91309@gmail.com>
2021-12-08 11:34:18 +0500

    something new

--- backend/App.js
+++ backend/App.js
@@ -27,10 +27,10 @@ app.use("/", user)
 app.use("/", order)
 app.use("/", payment)
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../fronttend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../fronttend/build/index.html"));
 });
 
 //middleWare for error
